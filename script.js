3ba6682191dc3c7660eb5f12d517d022aa71243f
Freja Jørgensen <freja@MacBook-Pro-4.local>
2019-05-26 21:55:15 +0200

    assignmentside færdig

--- script.js
+++ script.js
@@ -21,3 +21,11 @@ function toggleMenu() {
         document.querySelector("header .menu_toggle").textContent = "☰";
     }
 }
+
+
+document.querySelector(".theme .theme_descr").addEventListener("click", toggleAssignments);
+
+function toggleAssignments() {
+    console.log("toggleAssignments");
+    this.closest(".theme").classList.toggle("open");
+}
