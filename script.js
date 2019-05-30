be95bcae275b155292a2e7fd195d8d18b3bbed2a
Freja Jørgensen <freja@MacBook-Pro-4.local>
2019-05-30 16:35:32 +0200

    done

--- script.js
+++ script.js
@@ -4,28 +4,20 @@ function sidenVises() {
     console.log("sidenVises");
 
     document.querySelector("header .menu_toggle").addEventListener("click", toggleMenu);
-
-
 }
-
 
 function toggleMenu() {
     console.log("toggleMenu");
+
     document.querySelector("header nav").classList.toggle("active");
-
-    let erAktiv = document.querySelector("header nav").classList.contains("active");
-
-    if (erAktiv == true) {
-        document.querySelector("header .menu_toggle").textContent = "X";
-    } else {
-        document.querySelector("header .menu_toggle").textContent = "☰";
-    }
 }
 
+if (document.querySelector(".theme .theme_descr") !== null) {
+    document.querySelector(".theme .theme_descr").addEventListener("click", toggleAssignments);
 
-document.querySelector(".theme .theme_descr").addEventListener("click", toggleAssignments);
+    function toggleAssignments() {
+        console.log("toggleAssignments");
 
-function toggleAssignments() {
-    console.log("toggleAssignments");
-    this.closest(".theme").classList.toggle("open");
+        this.closest(".theme").classList.toggle("open");
+    }
 }
