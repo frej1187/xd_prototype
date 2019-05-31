368f0ebf71b9399a565bb8f09a666210e3d59512
Freja Jørgensen <freja@MacBook-Pro-4.local>
2019-05-31 16:56:40 +0200

    Klar til aflevering

--- script.js
+++ script.js
@@ -3,21 +3,28 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
     console.log("sidenVises");
 
-    document.querySelector("header .menu_toggle").addEventListener("click", toggleMenu);
+    // Hvis vi er på side der har .menu_toggle aktiverer vi eventlistener på .menu_toggle
+    if (document.querySelector("header .menu_toggle") !== null) {
+        // Laver eventlistener på .menu_toggle til at åbne/lukke menuen på mindre skærme
+        document.querySelector("header .menu_toggle").addEventListener("click", toggleMenu);
+    }
 }
 
 function toggleMenu() {
     console.log("toggleMenu");
 
+    // Toggler (skifter mellem at fjerne eller tilføje) class'en active på nav
     document.querySelector("header nav").classList.toggle("active");
 }
 
-if (document.querySelector(".theme .theme_descr") !== null) {
-    document.querySelector(".theme .theme_descr").addEventListener("click", toggleAssignments);
+// Hvis vi er på assignments siden aktiverer vi eventlistener på toggleAssignments
+if (document.querySelector(".theme_descr") !== null) {
+    document.querySelector(".theme_descr").addEventListener("click", toggleAssignments);
 
     function toggleAssignments() {
         console.log("toggleAssignments");
 
+        // Toggler (skifter mellem at fjerne eller tilføje) class'en open på den overliggende (forælder) med class'en theme
         this.closest(".theme").classList.toggle("open");
     }
 }
